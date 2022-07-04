247ad4428a6c1aafa88f2c27fefdca942ab8dd18
Lotte <lotteunckell@gmail.com>
2022-07-04 18:46:56 +0200

    add versioning script and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# mh_export_test
+Testing the export and versioning of JSON-Data using a shell script and GitHub
+
+1. Clone the repository.
+2. Check in lines 2 and 3 of the file `versioning.sh` if the URL of the modulehandbook in the variables `COURSES_URL` and `PROGRAMS_URL` are is correct.
+2. Run `bash versioning.sh` with semester shorthand as name for the tag.
---
+++ versioning.sh
@@ -0,0 +1,28 @@
+# usage: versioning.sh <push_to_git?> <semester_tag_name>
+COURSES_URL=https://module-handbook.herokuapp.com/export_courses_json
+PROGRAMS_URL=https://module-handbook.herokuapp.com/export_programs_json
+
+# download and save courses
+echo "Exporting Courses"
+curl ${COURSES_URL} > courses.json -H "Accept: application/json"
+echo "Exporting Courses Done"
+# download and save programs (with their courses)
+echo "Exporting Programs"
+curl ${PROGRAMS_URL} > programs.json -H "Accept: application/json"
+echo "Exporting Programs Done"
+
+# add
+git add .
+# commit
+git commit -m "version $(date)"
+# push
+git push origin main
+
+# Tag if tag given as a parameter
+if [ -n $1 ]
+then
+  # tag
+  git tag $1
+  # push
+  git push origin $1
+fi
